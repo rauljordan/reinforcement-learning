5ec89775ac41c72f4b39bb42d9ae4cd5c94074d0
Raul Jordan <raulitojordan@gmail.com>
2016-01-14 02:08:31 -0600

    added examples folder with grid mdp

---
+++ examples/gridMDP.js
@@ -0,0 +1,23 @@
+import {MDP} from '../';
+
+class GridMDP extends MDP {
+  constructor(grid, terminals, init = '(0, 0)', gamma = 0.9) {
+    super(init, [], terminals, gamma);
+    this.grid = grid;
+    this.rows = grid.length;
+    this.cols = grid[0].length;
+    for (var i = 0; i < this.rows; i++) {
+      for (var j = 0; j < this.cols; j++) {
+        this.reward[String([i,j])] = this.grid[i][j];
+        this.states.push([i, j]);
+      }
+    }
+  }
+}
+
+var mdp = new GridMDP([[-0.04, -0.04, -0.04, +1],
+                     [-0.04, -0.06,  -0.04, -1],
+                     [-0.04, -0.04, -0.04, -0.04]],
+                     [[3,2], [3,1]]);
+                     
+console.log(mdp.rows);
